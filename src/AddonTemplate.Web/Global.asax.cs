92faada067aeb1ec8d107e7687f7102b896736bb
steve leonard <sleonard84@msn.com>
2012-10-24 13:52:57 +0100

    request debuging

--- src/AddonTemplate.Web/Global.asax.cs
+++ src/AddonTemplate.Web/Global.asax.cs
@@ -1,4 +1,8 @@
-﻿using System.Web;
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using RestfulRouting;
@@ -24,5 +28,41 @@ namespace AddonTemplate.Web
             );
 			routes.MapRoutes<Routes>();
 		}
+
+        public void Application_BeginRequest(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = new SmtpClient("email-smtp.us-east-1.amazonaws.com", 587);
+
+               client.EnableSsl = true;
+
+               client.Credentials = new NetworkCredential("AKIAIETZDMTH5OPB3VMA", "AnHlEOWHR6qAaeX+tO5ajSLj1yHS/R+QBEcAAhTJp46O");
+
+                var sb = new StringBuilder();
+                sb.Append("<br/> Refer: " + Request.UrlReferrer);
+                sb.Append("<br/> Url: " + Request.Url.AbsoluteUri);
+                sb.Append("<br/> Query: " + Request.Url.Query);
+                foreach (var header in Request.Headers)
+                {
+                    sb.Append("<br/> Header: " + header + " val: " + Request.Headers[header.ToString()]);
+                }
+
+                var message = new MailMessage()
+                    {
+                        From = new MailAddress("sleonard84@msn.com", "Steve"),
+                        Subject = "Addon Debug Info",
+                        Body = sb.ToString(),
+                        IsBodyHtml=true
+                    };
+                message.To.Add(new MailAddress("sleonard84@msn.com", "Steve"));
+
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+              
+            }
+        }
 	}
 }
